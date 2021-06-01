a5588a7c95a2b6af45268617739af2347340266f
Jonathank479 <jonathan.krockhaus@student.jade-hs.de>
2021-06-02 01:56:26 +0200

    Create Innensechskantkopf.cs

---
+++ Schraubenprogramm/Innensechskantkopf.cs
@@ -0,0 +1,27 @@
+﻿internal class Innensechskantkopf
+{
+    public double zylinderdurchmesser { get; set; }
+    public double höhe { get; set; }
+    public double innenhöhe { get; set; }
+    public double innenschlüsselweite { get; set; }
+    public string art { get; set; }
+
+    public Innensechskantkopf(double zylinderdurchmesser, double höhe, double innehöhe, double innenschlüsselweite, string art)
+    {
+        this.zylinderdurchmesser = zylinderdurchmesser;
+        this.höhe = höhe;
+        this.innenhöhe = innehöhe;
+        this.innenschlüsselweite = innenschlüsselweite;
+        this.art = "Innensechskant";
+    }
+}
+    
+
+
+
+
+
+
+
+
+
