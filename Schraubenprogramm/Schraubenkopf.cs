35cc2a6b672ebf15ac322176187885dd76ae16b8
Jonathank479 <jonathan.krockhaus@student.jade-hs.de>
2021-06-02 01:55:18 +0200

    Create Schraubenkopf.cs

---
+++ Schraubenprogramm/Schraubenkopf.cs
@@ -0,0 +1,25 @@
+﻿internal class Schraubenkopf
+{
+    public double höhe { get; set; }
+    public double breite { get; set; }
+    public string art { get; set; }
+
+    public Schraubenkopf(double höhe, double breite, string art)
+    {
+        this.höhe = höhe;
+        this.breite = breite;
+        this.art = art;
+    }
+
+}
+   
+
+
+
+
+
+
+
+
+
+
